417d8a63052babcae735b916ec84c5b4dc5ac4e7
Guy Bryant <guy.s.bryant@gmail.com>
2018-11-05 03:59:53 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+  
+  def songs
+    Song.all.select {|artist| artist == self}
+  end
+  
+  def genres
+    Genre.all.select {|artist| artist == self}
+  end
+  
+end
