ecc444d3c60f73213eee3fe73d3915d587869c2c
Guy Bryant <guy.s.bryant@gmail.com>
2018-11-05 04:55:37 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,7 @@ class Patient
   end
   
   def appointments
-    Appointments.all.select {|appointment| appointment.patient == self
+    Appointments.all.select {|appointment| appointment.patient == self}
   end
   
   def doctors
